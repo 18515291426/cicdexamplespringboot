6726cdca3a602f98706e769d1d873c315305da7b
BAIJINLONG <baijinlong@kingsoft.com>
2019-10-21 11:41:31 +0800

    secret test

--- example-web/src/main/java/com/example/web/exampleweb/ExampleWebApplication.java
+++ example-web/src/main/java/com/example/web/exampleweb/ExampleWebApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExampleWebApplication {
 
     public static void main(String[] args) {
+
+
+
         SpringApplication.run(ExampleWebApplication.class, args);
     }
 
